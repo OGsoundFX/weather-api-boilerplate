a83e3fa3476cb5988a919bcd83515d4dcc3aed4d
Olivier Girardot <olive_girardot@hotmail.com>
2023-05-10 10:39:33 +0200

    created this boilerplate for a workshop

---
+++ javascript.js
@@ -0,0 +1,4 @@
+console.log("Hello World from the javascript.js file")
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=52.5200&lon=13.4050&appid=7b825c772cf3f2fbc4bb4a0673806a36"
+// url for icons: `https://openweathermap.org/img/w/${INSERT_ICON_ID_HERE}.png`
